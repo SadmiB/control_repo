5d942c37ff038e612eb605bc62a0bbc6cdf649d6
Sadmi Bouhafs <sadmibouhafs@gmail.com>
2022-01-25 10:15:18 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,5 +2,9 @@ node default {
   file {'/root/README.md':
     ensure => file,
     content => 'This is my readme',
+    owner => 'root',
+  }
+  file {'/root/README.md':
+    owner => 'root',
   }
 }
